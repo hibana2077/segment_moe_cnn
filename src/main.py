890c6b62fb49b743234346bd61631eaf7c62755a
hibana2077 <hibana2077@gmail.com>
2024-07-01 09:14:49 +0800

    feat: Update shuffle parameter in trainloader to improve data randomness

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ transform = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = DataLoader(trainset, batch_size=4, shuffle=True)
+trainloader = DataLoader(trainset, batch_size=4, shuffle=False)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = DataLoader(testset, batch_size=4, shuffle=False)
